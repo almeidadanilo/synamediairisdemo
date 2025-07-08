0cd9d0d5d94ec8fcc146fe836114a4eb8c9d3f16
Danilo de Almeida <danilo.almeida@gmail.com>
2025-07-08 15:05:42 -0300

    modified:   .gitignore 	new file:   others/PCTVLandmark2IrisScheduleTransformation/pctvLandmark2IrisSchedule.py 	modified:   others/genericADIContentIngestTransformation/ADIContent2IrisContent.py

--- others/genericADIContentIngestTransformation/ADIContent2IrisContent.py
+++ others/genericADIContentIngestTransformation/ADIContent2IrisContent.py
@@ -660,8 +660,8 @@ def wait(seconds):
 ######################################################################################################
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-input', type=str, default='Armageddon 2.xml',help='Go Tenant ID')
-parser.add_argument('-output', type=str, default='op7z4geq',help='Iris Tenant ID')
+parser.add_argument('-input', type=str, default='',help='Go Tenant ID')
+parser.add_argument('-output', type=str, default='',help='Iris Tenant ID')
 parser.add_argument('-log', type=str, default='file', choices=['console', 'file'], help='Log output destination')
 parser.add_argument('-level', type=str, default='debug', choices=['info', 'debug'], help='Log verbosity level')
 args = parser.parse_args()
